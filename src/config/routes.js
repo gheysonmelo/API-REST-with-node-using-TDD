2d03bbbf4ae63de81aeb5ded0121608a4c8a99ef
gheysonmelo <gheyson.melo@ufpe.br>
2022-11-23 01:37:53 -0300

    add: Added getAll method of accounts

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,5 @@
 module.exports = (app) => {
     app.route("/users").get(app.routes.users.findAll).post(app.routes.users.create);
 
-    app.route("/accounts").post(app.routes.accounts.create);
+    app.route("/accounts").get(app.routes.accounts.getAll).post(app.routes.accounts.create);
 };
--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,9 +1,15 @@
 module.exports = (app) => {
+    const getAll = (req, res) => {
+        app.services.account.findAll().then((result) => {
+            return res.status(200).json(result);
+        });
+    };
+
     const create = (req, res) => {
         app.services.account.save(req.body).then((result) => {
             return res.status(201).json(result[0]);
         });
     };
 
-    return { create };
+    return { getAll, create };
 };
