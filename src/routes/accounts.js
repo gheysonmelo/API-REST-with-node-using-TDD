f5e57833a365784699755d55cd6a8374e4821fc0
gheysonmelo <gheyson.melo@ufpe.br>
2022-12-18 23:56:46 -0300

    refactor: Managing errors in a generic way through middlewares

--- src/routes/accounts.js
+++ src/routes/accounts.js
@@ -1,33 +1,39 @@
 module.exports = (app) => {
-    const getAll = (req, res) => {
-        app.services.account.findAll().then((result) => {
-            return res.status(200).json(result);
-        });
-    };
-
-    const create = (req, res) => {
+    const create = (req, res, next) => {
         app.services.account
             .save(req.body)
             .then((result) => {
                 return res.status(201).json(result[0]);
             })
-            .catch((err) => res.status(400).json({ error: err.message }));
+            .catch((err) => next(err));
     };
 
-    const get = (req, res) => {
-        app.services.account.find({ id: req.params.id }).then((result) => {
-            return res.status(201).json(result);
-        });
+    const getAll = (req, res, next) => {
+        app.services.account
+            .findAll()
+            .then((result) => res.status(200).json(result))
+            .catch((err) => next(err));
     };
 
-    const update = (req, res) => {
+    const get = (req, res, next) => {
+        app.services.account
+            .find({ id: req.params.id })
+            .then((result) => res.status(201).json(result))
+            .catch((err) => next(err));
+    };
+
+    const update = (req, res, next) => {
         app.services.account
             .update(req.params.id, req.body)
-            .then((result) => res.status(200).json(result[0]));
+            .then((result) => res.status(200).json(result[0]))
+            .catch((err) => next(err));
     };
 
-    const remove = (req, res) => {
-        app.services.account.remove(req.params.id).then(() => res.status(204).send());
+    const remove = (req, res, next) => {
+        app.services.account
+            .remove(req.params.id)
+            .then(() => res.status(204).send())
+            .catch((err) => next(err));
     };
 
     return { getAll, create, get, update, remove };
